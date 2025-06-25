553a81581febb3a54c71e85d7e6c5069b2983800
Panda248 <p4nda248@gmail.com>
2025-06-25 12:00:53 -0400

    init

---
+++ MazeExplorer.h
@@ -0,0 +1,12 @@
+#pragma once
+/*
+* Header file for an automated maze explorer that contains struct and function declarations
+* functions include:
+* initialization/allocation/removal
+* maze solving algorithmn implementations
+* navigation
+* current state logging
+* 
+* would like to create:
+* user input navigation (no mouse just key input like old pov games)
+*/
---
+++ MazeGraph.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/*
+* Header file for MazeGraph C file
+* Contains maze_graph_t struct template and function declarations for operations with graphs intended to be used for maze generation
+* Operations of graphs include: 
+* graph initialization/allocation/removal
+* maze generation algorithmn implementations
+* access of nodes/edges
+* 
+*/
+
+typedef struct {
+
+}maze_graph_t;
